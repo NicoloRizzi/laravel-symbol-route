039b816be1cdeddc45c956f2fcfdd93f2a343222
Nicolo <nicolorizzi1996@gmail.com>
2020-06-11 08:55:48 +0200

    ADDED A NEW CONTENT IN A PAGE CONTACT

---
+++ blog/resources/views/contacts/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+@extends('layouts.header')
+@section('content')
+<div class="flex-center">
+    <p>Lorem ipsum</p>
+    <ul>
+        <li>Dolor</li>
+        <li>Sit</li>
+        <li>test</li>
+        <li>Test</li>
+    </ul>
+</div>
+@endsection
+@extends('layouts.footer')
